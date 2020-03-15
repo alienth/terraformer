ca6ed493bf9eef32523928e5b327708a74de78de
Jason Harvey <alienth@gmail.com>
2020-03-14 17:36:42 -0800

    fixup.

--- cmd/provider_cmd_wavefront.go
+++ cmd/provider_cmd_wavefront.go
@@ -1,16 +1,3 @@
-// Copyright 2019 The Terraformer Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 package cmd
 
 import (
@@ -35,11 +22,11 @@ func newCmdWavefrontImporter(options ImportOptions) *cobra.Command {
 		},
 	}
 
-	cmd.AddCommand(listCmd(newNewRelicProvider()))
+	cmd.AddCommand(listCmd(newWavefrontProvider()))
 	baseProviderFlags(cmd.PersistentFlags(), &options, "alert", "newrelic_dashboard=id1:id2:id4")
 	return cmd
 }
 
-func wavefrontProvider() terraform_utils.ProviderGenerator {
+func newWavefrontProvider() terraform_utils.ProviderGenerator {
 	return &wavefront_terraforming.WavefrontProvider{}
 }
